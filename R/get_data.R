de729785d842946d17ec2e1e913342f0804d3ad4
Shians <registertonysu@gmail.com>
2017-12-05 13:26:57 +1100

    Refactored data extraction

--- R/get_data.R
+++ R/get_data.R
@@ -1,51 +1,60 @@
-library(BiocPkgTools)
-library(stringr)
-library(dplyr)
-library(jsonlite)
-library(readr)
+get_bioc_data <- function() {
+    pkg_list <- BiocPkgTools::getBiocPkgList()
+    raw_dl_stats <- BiocPkgTools::getBiocDownloadStats()
 
-pkg_list <- BiocPkgTools::getBiocPkgList()
-dl_stats <- BiocPkgTools::getBiocDownloadStats()
+    full_data <- process_data(pkg_list, raw_dl_stats)
 
-dl_stats <- dl_stats %>%
-    group_by(Package) %>%
-    summarise(
-        downloads_month = first(Nb_of_downloads),
-        downloads_total = sum(Nb_of_downloads)
-    )
-
-pkg_link <- function(pkg) {
-    str_interp("http://bioconductor.org/packages/release/bioc/html/${pkg}.html")
+    jsonlite::toJSON(full_data)
 }
 
-full_data <- inner_join(pkg_list, dl_stats, by = "Package") %>%
-    select(
-        Author,
-        Package,
-        License,
-        biocViews,
-        Description,
-        downloads_month,
-        downloads_total
-    ) %>%
-    mutate(
-        page = pkg_link(Package),
-        package = str_split(biocViews, ", ")
-    ) %>%
-    rename (
-        authors = Author,
-        name = Package,
-        license = License,
-        tags = biocViews,
-        description = Description
-    )
+process_data <- function(raw_dl_stats, pkg_list) {
+    dl_stats <- summarise_dl_stats(raw_dl_stats)
 
-collapse_list <- function(x) unlist(x) %>% paste(collapse = ", ")
+    pkg_link <- function(pkg) {
+        stringr::str_interp(
+            "http://bioconductor.org/packages/release/bioc/html/${pkg}.html"
+        )
+    }
 
-full_data$authors <- sapply(full_data$authors, collapse_list) %>%
-    str_replace(" and ", ", ") %>%
-    str_replace(",([^,]*)$", " and\\1")
+    full_data <- dplyr::inner_join(pkg_list, dl_stats, by = "Package") %>%
+        dplyr::select(
+            Author,
+            Package,
+            License,
+            biocViews,
+            Description,
+            downloads_month,
+            downloads_total
+        ) %>%
+        dplyr::mutate(
+            page = pkg_link(Package),
+            package = stringr::str_split(biocViews, ", ")
+        ) %>%
+        dplyr::rename(
+            authors = Author,
+            name = Package,
+            license = License,
+            tags = biocViews,
+            description = Description
+        )
 
-json <- toJSON(full_data)
+    full_data$authors <- author_list_to_string(full_data$authors)
 
-write_file(paste("var data =", json), "bioC.json")
+    full_data
+}
+
+summarise_dl_stats <- function(dl_stats) {
+    dl_stats %>%
+        dplyr::group_by(Package) %>%
+        dplyr::summarise(
+            downloads_month = dplyr::first(Nb_of_downloads),
+            downloads_total = sum(Nb_of_downloads)
+        )
+}
+
+author_list_to_string <- function(authors) {
+    collapse_list <- function(x) unlist(x) %>% paste(collapse = ", ")
+    sapply(authors, collapse_list) %>%
+        str_replace(" and ", ", ") %>%
+        str_replace(",([^,]*)$", " and\\1")
+}
