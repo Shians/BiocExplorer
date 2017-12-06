7fa1133f52b6956b858eb731944285cc8b55f487
Shians <registertonysu@gmail.com>
2017-12-06 13:06:18 +1100

    Added instructions

--- R/bioc_explore.R
+++ R/bioc_explore.R
@@ -1,6 +1,10 @@
 #' @import htmlwidgets
 #' @export
 bioc_explore <- function(width = NULL, height = NULL, elementId = NULL) {
+    # instruction messages
+    message("- Hover over bubbles to see full name and lifetime downloads")
+    message("- Click on bubbles to see more information about package")
+    message("- Use filter to filter by biocViews")
 
     # read the gexf file
     data <- get_bioc_data()
